47b5eac62adb2e6fb06c8cec58f4ff973cc16eb4
Droller <drollercanvas@gmail.com>
2026-09-23 17:14:38 +0545

    Add files via upload
    I figured out how to find the third largest number in an array.

---
+++ array_third_largest.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main() {
+    int array[5] = {2, 1, 4, 3, 5};
+    int length = sizeof(array) / sizeof(array[0]);
+
+    int largest = array[0];
+    int second_largest = array[1];
+	int third_largest = array[2];
+	int temp1 = largest;
+	int temp2 = second_largest;
+	int temp3 = third_largest;
+	
+	if (largest > second_largest && largest > third_largest){
+		if(second_largest > third_largest){
+			
+		}else{
+			second_largest = third_largest;
+			third_largest = temp2;
+		}
+	}else if(second_largest > largest && second_largest > third_largest){
+		largest = second_largest;
+		second_largest = temp1;
+		if(second_largest > third_largest){
+			
+		}else if(third_largest > second_largest){
+			second_largest = third_largest;
+			third_largest = temp2;
+		}
+	}else{
+		largest = third_largest;
+		third_largest = temp1;
+		if(third_largest > second_largest){
+			second_largest = third_largest;
+			third_largest = temp2;
+		}
+	}
+	
+	for(int x = 3; x < length; x++){
+		if(array[x] > largest){
+			third_largest = second_largest;
+			second_largest = largest;
+			largest = array[x];
+		} else if (array[x] > second_largest){
+			third_largest = second_largest;
+			second_largest = array[x];
+		}else if (array[x] > third_largest){
+			third_largest = array[x];
+		}
+	}
+	
+    printf("%d", third_largest);
+    
+	/*  Done by my own wit, I'm so proud of this lol.
+		Needs alot of optimization for sure though.  */	
+    return 0;
+}
